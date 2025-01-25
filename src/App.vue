549f40a3c0c3b2fb946f1db07bc0b2508eba01b1
Ealsen_Huangfu <111371035+Ealsen@users.noreply.github.com>
2025-01-25 17:56:23 +0800

    新增

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@ import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import ifDemo from './components/ifDemo.vue'
 import itemDemo from './components/itemDemo.vue'
-import keyDemo from './components/keyDemo.vue';
-import eventDemo1 from './components/eventDemo1.vue';
-import eventDemo2 from './components/eventDemo2.vue';
-import eventDemo3 from './components/eventDemo3.vue';
-import eventDemo5 from './components/eventDemo5.vue';
+import keyDemo from './components/keyDemo.vue'
+import eventDemo1 from './components/eventDemo1.vue'
+import eventDemo2 from './components/eventDemo2.vue'
+import eventDemo3 from './components/eventDemo3.vue'
+import eventDemo5 from './components/eventDemo5.vue'
+import eventDemo6 from './components/eventDemo6.vue'
 
 </script>
 
@@ -20,6 +21,7 @@ import eventDemo5 from './components/eventDemo5.vue';
     <eventDemo2 />
     <eventDemo3 />
     <eventDemo5 />
+    <eventDemo6 />
 </template>
 
 
---
+++ src/components/eventDemo6.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'eventDemo6',
+        }
+    },
+    methods: {
+        handleClick(event) {
+            console.log(event);
+        },
+        clickHandle(e) {
+            // e.preventDefault(); // 阻止默认事件
+            console.log('click handle');
+        },
+        clickDiv() {
+            console.log('click div');
+        },
+        clickP(e) {
+            e.stopPropagation(); // 阻止冒泡
+            console.log('click p');
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <h3>事件修饰符</h3>
+        <a @click.prevent="clickHandle" href="https://www.baidu.com" @click.stop="handleClick">百度</a>
+    </div>
+    <div>
+    <h3>测试冒泡</h3>
+    <div @click="clickDiv">
+    <p @click.stop="clickP">点击我，我会阻止冒泡</p>
+    </div>
+    </div>
+</template>
