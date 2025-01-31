bbe3259fb516d958ef0e1448263d06378e9edda7
Ealsen_Huangfu <111371035+Ealsen@users.noreply.github.com>
2025-01-31 17:23:56 +0800

    组件数据传递子传父Props

--- study-first/src/components/componentADemo.vue
+++ study-first/src/components/componentADemo.vue
@@ -2,7 +2,17 @@
     <div>
         <h3>组件传递Props校验</h3>
         <p>{{ message }}</p>
-        <componentBDemo :data1="data1" />
+        <componentBDemo :data1="data1" @someEvent="handleSomeEvent" @searchDataEvent="handleSearchDataEvent"
+            :onEvent="dataFn" />
+        <p>父元素：{{ emitData }}</p>
+    </div>
+    <div>
+        <h3>A组件事件配合v-model</h3>
+        <p>搜索结果: {{ emitSearchData }}</p>
+    </div>
+    <div>
+        <h3>组件数据传递子传父Props</h3>
+        <p>子组件传递给父组件的数据：{{ data3 }}</p>
     </div>
 </template>
 
@@ -14,10 +24,28 @@ export default {
         return {
             message: 'Hello from the componentA',
             data1: 'A data1',
+            emitData: '',
+            emitSearchData: '',
+            data3: "",
         }
     },
     components: {
         componentBDemo,
-    }
+    },
+    methods: {
+        handleSomeEvent(data) {
+            console.log('handleSomeEvent', data);
+            this.emitData = data;
+        },
+        handleSearchDataEvent(data1) {
+            console.log('handleSearchDataEvent', data1);
+            this.emitSearchData = data1;
+        },
+        dataFn(data3) {
+            console.log('dataFn => ', data3);
+            this.data3 = data3;
+        },
+    },
+
 }
 </script>
--- study-first/src/components/componentBDemo.vue
+++ study-first/src/components/componentBDemo.vue
@@ -2,6 +2,12 @@
     <div>
         <p>{{ message }}</p>
         <p>{{ data1 }}</p>
+        <h3>组件事件</h3>
+        <button @click="handleClick1">$emit传递数据给componentA</button>
+        <h3>B组件事件配合v-model</h3>
+        搜索:<input type="text" v-model="searchData" />
+        <p>搜索结果: {{ searchData }}</p>
+        <p>{{ onEvent('event from componentB') }}</p>
     </div>
 </template>
 
@@ -9,9 +15,22 @@
 export default {
     data() {
         return {
-            message: 'Hello from the componentB'
+            message: 'Hello from the componentB',
+            dataMsg: "data from componentB",
+            searchData: "",
         }
     },
-    props: { 'data1': { type: String }, },
-}
+    props: { 'data1': { type: String }, onEvent: { type: Function } },
+    methods: {
+        handleClick1() {
+            this.$emit('someEvent', this.dataMsg);
+        }
+    },
+    watch: {
+        searchData(newVal, oldVal) {
+            console.log(`Message changed: ${oldVal} => ${newVal}`);
+            this.$emit('searchDataEvent', newVal);
+        }
+    }
+}   
 </script>
