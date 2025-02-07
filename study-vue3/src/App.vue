1c0f09674c238799c74334ef719c5f1460371ec3
Ealsen_Huangfu <111371035+Ealsen@users.noreply.github.com>
2025-02-07 23:09:57 +0800

    feat: 添加App组件及相关样式

---
+++ study-vue3/src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref, reactive, provide } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        RouterLink,
+        RouterView,
+        HelloWorld,
+    },
+    setup() {
+
+        const msg = ref('Hello Vue 3!')
+
+        const object_data_1 = reactive({
+            fruitsList: ["apple", "banana", "orange"]
+        })
+
+        const dataFromParent = provide('dataFromParent', "I am data from parent")
+
+        return {
+            msg,
+            object_data_1,
+            dataFromParent,
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="App">
+        <h1>Hello World</h1>
+        <h3>{{ msg }}</h3>
+        <HelloWorld msg="I from the App component!" />
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+.App {
+    border: black 1px solid;
+    background-color: #ffffff;
+    padding: 10px;
+    border-radius: 5px;
+    margin: 30px auto;
+}
+</style>
